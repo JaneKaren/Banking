c10172757f5b40d9b13a54cf13ab8f445ee758f2
Jane Karen <janekaren@soukroma.cy>
2011-02-17 16:06:35 +0100

    People table

---
+++ db/migrate/170_add_moves_people.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8 
+class AddMovesPeople < ActiveRecord::Migration
+  def self.up
+    moves = Move.all
+    for move in moves do
+      if person = Person.find_by_name(move.notice.squish)
+        move.person_id = person.id
+        move.save
+      end
+    end
+  end
+
+  def self.down
+  end
+end
